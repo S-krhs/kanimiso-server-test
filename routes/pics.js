c03d9cbb702a0ab244266297e6fee53b83416073
S_Kurahashi <86878523+S-krhs@users.noreply.github.com>
2023-02-12 09:15:56 +0900

    aws rds conn

--- routes/pics.js
+++ routes/pics.js
@@ -5,9 +5,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', async(req, res, next) => {
+
   const prisma = new PrismaClient();
-  const allPics = await prisma.test.findMany();
-  res.json(allPics);
+
+  try{
+
+    console.log("データベースとの接続を開始します。")
+    const allPics = await prisma.test.findMany();
+    res.json(allPics);
+    console.log("データの取得に成功しました。")
+
+  }catch(e){
+
+    console.log("データの取得に失敗しました。:",e);
+
+  }finally{
+
+    prisma.$disconnect();
+    console.log("データベースとの接続を終了しました。");
+
+  }
+
 });
 
 module.exports = router;
