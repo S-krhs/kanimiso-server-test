82a9ccf81ecafb17e1e8247b4341a7dee3f686b3
S_Kurahashi <86878523+S-krhs@users.noreply.github.com>
2023-03-06 13:50:06 +0900

    2023/03/06

--- routes/pics.js
+++ routes/pics.js
@@ -1,15 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const router = express.Router();
-//const Memcached = require('memcached');
 
 const prisma = new PrismaClient();
-//const memcachedClient = new Memcached('localhost:11211');
 
-
-//const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-//const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-//const { PollyCustomizations } = require('aws-sdk/lib/services/polly');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
@@ -27,27 +21,6 @@ router.get('/', async (req, res) => {
     res.send(data);
 
     // ToDo:Cache
-
-    // const imageArray = await Promise.all(
-    //   images.map(async (image) => {
-    //     const params = {
-    //       Bucket: process.env.BUCKET_NAME,
-    //       Key: decodeURI(image.path)
-    //     };
-    //     const imageData = await s3.getObject(params).promise();
-
-    //     return {
-    //       "name":image.timestamp,
-    //       "body": imageData.Body
-    //     };
-    //   })
-    // );
-
-    //const imageArrayJson = JSON.stringify(imageArray);
-
-    
-    //res.send(imageArrayJson);
-    //res.send(images);
 
   } catch (e) {
     console.log("データの取得に失敗しました。:", e);
