9a7be6210f2212879e8d582d84cf409758832e0f
S_Kurahashi <86878523+S-krhs@users.noreply.github.com>
2023-03-06 13:48:11 +0900

    gitignore change

--- prisma/migrations/20230211094553_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "test" (
-    "id" SERIAL NOT NULL,
-    "alt" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
-
-    CONSTRAINT "test_pkey" PRIMARY KEY ("id")
-);
